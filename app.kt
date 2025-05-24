60b5fb5b58ba5aeec6be09114c37a4740c046cc4
babarai45 <muhammadbabarai45@gmail.com>
2025-05-24 20:29:02 +0500

    start new journy

---
+++ app.kt
@@ -0,0 +1,6 @@
+val name = "John Doe"
+
+fun main() {
+    println("Hello, $name!")
+}
+// This is a simple Kotlin program that prints a greeting message to the console.
---
+++ app.rs
@@ -0,0 +1,7 @@
+fn main() {
+    let name = "John Doe";
+    let lname = "Muhammd BABAR";
+    println!("Hello, {}!", name);
+    println!("Hy Wellcome, {}!", lname);
+}
+// This is a simple Rust program that prints a greeting message to the console.
